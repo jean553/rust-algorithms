d4577f5b3bfefc8633911a30b95d613d38aa7459
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2018-02-03 10:23:13 +0100

    integrate min tests for u8 and u16

--- rust-algorithms/src/tests_min.rs
+++ rust-algorithms/src/tests_min.rs
@@ -1,16 +1,34 @@
 #[cfg(test)]
 mod tests {
 
-    use lib;
+    use lib::min;
 
     #[test]
-    fn test_min() {
+    fn test_min_with_u8() {
 
-        let array: [u8; 6] = [3, 5, 6, 1, 7, 4];
-        assert_eq!(
-            lib::min(&array),
-            1,
-            "Unexpected minimum value",
-        );
+        let array: [u8; 6] = [
+            255,
+            125,
+            78,
+            140,
+            88,
+            87,
+        ];
+
+        assert_eq!(min(&array), 78);
+    }
+
+    #[test]
+    fn test_min_with_u16() {
+
+        let array: [u16; 6] = [
+            60000,
+            32546,
+            10000,
+            65535,
+            222,
+            8909,
+        ];
+        assert_eq!(min(&array), 222);
     }
 }
