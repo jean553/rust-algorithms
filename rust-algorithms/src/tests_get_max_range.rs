02c8af6c412eddecde48e536978b3321c341c49a
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2018-02-03 13:58:03 +0100

    fix tests syntax

--- rust-algorithms/src/tests_get_max_range.rs
+++ rust-algorithms/src/tests_get_max_range.rs
@@ -6,16 +6,7 @@ mod tests {
     #[test]
     fn test_get_max_range() {
 
-        let array: [i32; 7] = [
-            -15,
-            110,
-            78,
-            32,
-            -45,
-            120,
-            -50,
-        ];
-
+        let array: [i32; 7] = [-15, 110, 78, 32, -45, 120, -50];
         assert_eq!(get_max_range(&array), 170);
 
         let array: [i32; 7] = [0, 1, -2, 0, 3, 2, 5];
@@ -25,16 +16,7 @@ mod tests {
     #[test]
     fn test_get_max_range_only_signed() {
 
-        let array: [i32; 7] = [
-            -15,
-            -110,
-            -78,
-            -32,
-            -45,
-            -120,
-            -50,
-        ];
-
+        let array: [i32; 7] = [-15, -110, -78, -32, -45, -120, -50];
         assert_eq!(get_max_range(&array), 105);
     }
 }
