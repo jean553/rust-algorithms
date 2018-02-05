9bc0f806f41103b850f38666d1cff5d033087e5c
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2018-02-05 21:11:48 +0100

    define the tests for the function that list all the permutations with repetitions

--- rust-algorithms/src/tests_permutations_with_repetitions.rs
+++ rust-algorithms/src/tests_permutations_with_repetitions.rs
@@ -1,7 +1,12 @@
 #[cfg(test)]
 mod tests {
 
-    use lib::permutations_with_repetitions;
+    use lib::{
+        permutations_with_repetitions,
+        get_all_permutations_with_repetitions,
+    };
+
+    use std::collections::HashSet;
 
     #[test]
     fn test_permutations_with_repetitions() {
@@ -15,6 +20,25 @@ mod tests {
                 selection_amount,
             ),
             10000,
+        );
+    }
+
+    #[test]
+    fn test_print_all_permutations_with_repetitions() {
+
+        let array: [u8; 2] = [1, 2];
+
+        let all_permutations = get_all_permutations_with_repetitions(&array);
+
+        let mut result: HashSet<Vec<u8>> = HashSet::new();
+        result.insert(vec![1, 1]);
+        result.insert(vec![1, 2]);
+        result.insert(vec![2, 1]);
+        result.insert(vec![2, 2]);
+
+        assert_eq!(
+            all_permutations,
+            result,
         );
     }
 }
