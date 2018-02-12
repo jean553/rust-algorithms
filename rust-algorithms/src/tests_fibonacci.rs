23bc44fd2dafd970ce58dec6f80d5a971d584fd6
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2018-02-12 13:30:09 +0100

    integrate one test to check fibonacci definition with memoization

--- rust-algorithms/src/tests_fibonacci.rs
+++ rust-algorithms/src/tests_fibonacci.rs
@@ -1,7 +1,10 @@
 #[cfg(test)]
 mod tests {
 
-    use lib::fibonacci_recursive;
+    use lib::{
+        fibonacci_recursive,
+        fibonacci_recursive_with_memoization,
+    };
 
     #[test]
     fn test_fibonacci_recursive() {
@@ -9,6 +12,15 @@ mod tests {
         assert_eq!(
             fibonacci_recursive(6),
             8, // 0, 1, 1, 2, 3, 5, 8
+        );
+    }
+
+    #[test]
+    fn test_fibonacci_recursive_with_memoization() {
+
+        assert_eq!(
+            fibonacci_recursive_with_memoization(6),
+            8,
         );
     }
 }
